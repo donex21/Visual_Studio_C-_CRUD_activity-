80839f82577fca02e25950898b196746cf5c42a5
donex21 <donilponce@gmail.com>
2020-03-27 17:12:08 +0800

    Firs Commit

---
+++ Act_1_CRUD_StudentInfo/Act_1_CRUD_StudentInfo/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Act_1_CRUD_StudentInfo
+{
+    partial class StudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.IDtextBox = new System.Windows.Forms.TextBox();
+            this.FNametextBox = new System.Windows.Forms.TextBox();
+            this.MNametextBox = new System.Windows.Forms.TextBox();
+            this.LNametextBox = new System.Windows.Forms.TextBox();
+            this.AddresstextBox = new System.Windows.Forms.TextBox();
+            this.GendertextBox = new System.Windows.Forms.TextBox();
+            this.AgetextBox = new System.Windows.Forms.TextBox();
+            this.CoursetextBox = new System.Windows.Forms.TextBox();
+            this.YeartextBox = new System.Windows.Forms.TextBox();
+            this.Createbutton = new System.Windows.Forms.Button();
+            this.Retrievebutton = new System.Windows.Forms.Button();
+            this.Updatebutton = new System.Windows.Forms.Button();
+            this.Deletebutton = new System.Windows.Forms.Button();
+            this.BDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(385, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sudent Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FirstName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MIddle Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "LastName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(41, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Address";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(424, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Age";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(54, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "BDate";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(406, 146);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Course";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(404, 84);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Gender";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(420, 173);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(37, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Year";
+            // 
+            // IDtextBox
+            // 
+            this.IDtextBox.Location = new System.Drawing.Point(118, 51);
+            this.IDtextBox.Name = "IDtextBox";
+            this.IDtextBox.Size = new System.Drawing.Size(100, 20);
+            this.IDtextBox.TabIndex = 11;
+            // 
+            // FNametextBox
+            // 
+            this.FNametextBox.Location = new System.Drawing.Point(118, 83);
+            this.FNametextBox.Name = "FNametextBox";
+            this.FNametextBox.Size = new System.Drawing.Size(100, 20);
+            this.FNametextBox.TabIndex = 12;
+            // 
+            // MNametextBox
+            // 
+            this.MNametextBox.Location = new System.Drawing.Point(118, 114);
+            this.MNametextBox.Name = "MNametextBox";
+            this.MNametextBox.Size = new System.Drawing.Size(100, 20);
+            this.MNametextBox.TabIndex = 13;
+            // 
+            // LNametextBox
+            // 
+            this.LNametextBox.Location = new System.Drawing.Point(118, 145);
+            this.LNametextBox.Name = "LNametextBox";
+            this.LNametextBox.Size = new System.Drawing.Size(100, 20);
+            this.LNametextBox.TabIndex = 14;
+            // 
+            // AddresstextBox
+            // 
+            this.AddresstextBox.Location = new System.Drawing.Point(118, 172);
+            this.AddresstextBox.Name = "AddresstextBox";
+            this.AddresstextBox.Size = new System.Drawing.Size(100, 20);
+            this.AddresstextBox.TabIndex = 15;
+            // 
+            // GendertextBox
+            // 
+            this.GendertextBox.Location = new System.Drawing.Point(463, 83);
+            this.GendertextBox.Name = "GendertextBox";
+            this.GendertextBox.Size = new System.Drawing.Size(100, 20);
+            this.GendertextBox.TabIndex = 17;
+            // 
+            // AgetextBox
+            // 
+            this.AgetextBox.Location = new System.Drawing.Point(463, 114);
+            this.AgetextBox.Name = "AgetextBox";
+            this.AgetextBox.Size = new System.Drawing.Size(100, 20);
+            this.AgetextBox.TabIndex = 18;
+            // 
+            // CoursetextBox
+            // 
+            this.CoursetextBox.Location = new System.Drawing.Point(463, 145);
+            this.CoursetextBox.Name = "CoursetextBox";
+            this.CoursetextBox.Size = new System.Drawing.Size(100, 20);
+            this.CoursetextBox.TabIndex = 19;
+            // 
+            // YeartextBox
+            // 
+            this.YeartextBox.Location = new System.Drawing.Point(463, 172);
+            this.YeartextBox.Name = "YeartextBox";
+            this.YeartextBox.Size = new System.Drawing.Size(100, 20);
+            this.YeartextBox.TabIndex = 20;
+            // 
+            // Createbutton
+            // 
+            this.Createbutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Createbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Createbutton.Location = new System.Drawing.Point(699, 67);
+            this.Createbutton.Name = "Createbutton";
+            this.Createbutton.Size = new System.Drawing.Size(87, 33);
+            this.Createbutton.TabIndex = 21;
+            this.Createbutton.Text = "&Create";
+            this.Createbutton.UseVisualStyleBackColor = false;
+            this.Createbutton.Click += new System.EventHandler(this.Createbutton_Click);
+            // 
+            // Retrievebutton
+            // 
+            this.Retrievebutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Retrievebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Retrievebutton.Location = new System.Drawing.Point(699, 115);
+            this.Retrievebutton.Name = "Retrievebutton";
+            this.Retrievebutton.Size = new System.Drawing.Size(87, 33);
+            this.Retrievebutton.TabIndex = 22;
+            this.Retrievebutton.Text = "&Retrieve";
+            this.Retrievebutton.UseVisualStyleBackColor = false;
+            this.Retrievebutton.Click += new System.EventHandler(this.Retrievebutton_Click);
+            // 
+            // Updatebutton
+            // 
+            this.Updatebutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Updatebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Updatebutton.Location = new System.Drawing.Point(822, 67);
+            this.Updatebutton.Name = "Updatebutton";
+            this.Updatebutton.Size = new System.Drawing.Size(87, 33);
+            this.Updatebutton.TabIndex = 23;
+            this.Updatebutton.Text = "&Update";
+            this.Updatebutton.UseVisualStyleBackColor = false;
+            this.Updatebutton.Click += new System.EventHandler(this.Updatebutton_Click);
+            // 
+            // Deletebutton
+            // 
+            this.Deletebutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Deletebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Deletebutton.Location = new System.Drawing.Point(822, 115);
+            this.Deletebutton.Name = "Deletebutton";
+            this.Deletebutton.Size = new System.Drawing.Size(87, 33);
+            this.Deletebutton.TabIndex = 24;
+            this.Deletebutton.Text = "&Delete";
+            this.Deletebutton.UseVisualStyleBackColor = false;
+            this.Deletebutton.Click += new System.EventHandler(this.Deletebutton_Click);
+            // 
+            // BDate
+            // 
+            this.BDate.Location = new System.Drawing.Point(118, 198);
+            this.BDate.Name = "BDate";
+            this.BDate.Size = new System.Drawing.Size(200, 20);
+            this.BDate.TabIndex = 25;
+            // 
+            // StudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 248);
+            this.Controls.Add(this.BDate);
+            this.Controls.Add(this.Deletebutton);
+            this.Controls.Add(this.Updatebutton);
+            this.Controls.Add(this.Retrievebutton);
+            this.Controls.Add(this.Createbutton);
+            this.Controls.Add(this.YeartextBox);
+            this.Controls.Add(this.CoursetextBox);
+            this.Controls.Add(this.AgetextBox);
+            this.Controls.Add(this.GendertextBox);
+            this.Controls.Add(this.AddresstextBox);
+            this.Controls.Add(this.LNametextBox);
+            this.Controls.Add(this.MNametextBox);
+            this.Controls.Add(this.FNametextBox);
+            this.Controls.Add(this.IDtextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentInfo";
+            this.Text = "Student Info (CRUD)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox IDtextBox;
+        private System.Windows.Forms.TextBox FNametextBox;
+        private System.Windows.Forms.TextBox MNametextBox;
+        private System.Windows.Forms.TextBox LNametextBox;
+        private System.Windows.Forms.TextBox AddresstextBox;
+        private System.Windows.Forms.TextBox GendertextBox;
+        private System.Windows.Forms.TextBox AgetextBox;
+        private System.Windows.Forms.TextBox CoursetextBox;
+        private System.Windows.Forms.TextBox YeartextBox;
+        private System.Windows.Forms.Button Createbutton;
+        private System.Windows.Forms.Button Retrievebutton;
+        private System.Windows.Forms.Button Updatebutton;
+        private System.Windows.Forms.Button Deletebutton;
+        private System.Windows.Forms.DateTimePicker BDate;
+    }
+}
+
---
+++ Act_1_CRUD_StudentInfo/Act_1_CRUD_StudentInfo/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Act_1_CRUD_StudentInfo
+{
+    public partial class StudentInfo : Form
+    {
+        private static string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\donilfiles\AIS\SutudentInfo.mdb";
+        OleDbConnection con = new OleDbConnection(connectionString);
+        public StudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void Createbutton_Click(object sender, EventArgs e)
+        {
+            if (!AllTextEmpty(this))
+            {
+                if (ifValidNumber(AgetextBox.Text) && ifValidNumber(YeartextBox.Text) && ifValidNumber(IDtextBox.Text))
+                {
+                    string sql = "Select * From StudentInfo";
+                    OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, con);
+                    OleDbCommandBuilder cmdbuilder = new OleDbCommandBuilder(dataAdapter);
+                    DataSet dataset = new DataSet();
+                    dataAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                    dataAdapter.Fill(dataset, "StudentInfo");
+
+                    DataRow findrow = dataset.Tables["StudentInfo"].Rows.Find(IDtextBox.Text);
+                    if (findrow == null)
+                    {
+                        DataRow datarow = dataset.Tables["StudentInfo"].NewRow();
+                        datarow[0] = Convert.ToInt32(IDtextBox.Text);
+                        datarow[1] = FNametextBox.Text;
+                        datarow[2] = MNametextBox.Text;
+                        datarow[3] = LNametextBox.Text;
+                        datarow[4] = AddresstextBox.Text;
+                        datarow[5] = Convert.ToDateTime(BDate.Text);
+                        datarow[6] = GendertextBox.Text;
+                        datarow[7] = Convert.ToInt32(AgetextBox.Text);
+                        datarow[8] = CoursetextBox.Text;
+                        datarow[9] = Convert.ToInt32(YeartextBox.Text);
+
+                        dataset.Tables["StudentInfo"].Rows.Add(datarow);
+                        dataAdapter.Update(dataset, "StudentInfo");
+                        MessageBox.Show("Successfully Saved");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Duplicate Entry");
+                    }
+                    ClearAllText(this);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Input");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pls. Complete All Details");
+            }
+        }
+
+        //Method Clear All Textbox
+        public void ClearAllText(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Clear();
+                else
+                    ClearAllText(c);
+            }
+        }
+
+        public bool ifValidNumber(String Value)
+        {
+            try
+            {
+                double val = Convert.ToInt32(Value);
+                return true;
+            }
+            catch
+            {            
+                return false;
+            }
+        }
+        //All textbox is Empty
+        public bool AllTextEmpty(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                if (c is TextBox)
+                    if (((TextBox)c).Text == "")
+                        return true;
+            }
+            return false;
+        }
+
+        private void Retrievebutton_Click(object sender, EventArgs e)
+        {
+            bool found = false;
+            con.Open();
+            OleDbCommand command = con.CreateCommand();
+            command.CommandText = "SELECT * FROM StudentInfo";
+            OleDbDataReader thisreader = command.ExecuteReader();
+            while (thisreader.Read())
+            {
+                if (thisreader["ID"].ToString() == IDtextBox.Text)
+                {
+                    found = true;
+                    FNametextBox.Text = thisreader["FName"].ToString();
+                    MNametextBox.Text = thisreader["MName"].ToString();
+                    LNametextBox.Text = thisreader["LName"].ToString();
+                    AddresstextBox.Text = thisreader["Address"].ToString();
+                    BDate.Text =thisreader["BDate"].ToString();
+                    GendertextBox.Text = thisreader["Gender"].ToString();
+                    AgetextBox.Text = thisreader["Age"].ToString();
+                    CoursetextBox.Text = thisreader["Course"].ToString();
+                    YeartextBox.Text = thisreader["yr"].ToString();
+                    break;
+                }
+            }
+            thisreader.Close();
+            con.Close();
+            if (!found)
+            {              
+                MessageBox.Show("Student ID Not found");
+                IDtextBox.Clear();
+            }
+        }
+
+        private void Updatebutton_Click(object sender, EventArgs e)
+        {
+            string sql = "Select * From StudentInfo";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, con);
+            OleDbCommandBuilder cmdbuilder = new OleDbCommandBuilder(dataAdapter);
+            DataSet dataset = new DataSet();
+            dataAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            dataAdapter.Fill(dataset, "StudentInfo");
+
+            DataRow findrow = dataset.Tables["StudentInfo"].Rows.Find(IDtextBox.Text);
+            if (findrow != null)
+            {
+               // DataRow datarow = dataset.Tables["StudentInfo"].NewRow();
+                findrow[0] = Convert.ToInt32(IDtextBox.Text);
+                findrow[1] = FNametextBox.Text;
+                findrow[2] = MNametextBox.Text;
+                findrow[3] = LNametextBox.Text;
+                findrow[4] = AddresstextBox.Text;
+                findrow[5] = Convert.ToDateTime(BDate.Text);
+                findrow[6] = GendertextBox.Text;
+               // findrow[7] = Convert.ToInt32(AgetextBox.Text);
+                findrow[8] = CoursetextBox.Text;
+                //findrow[9] = Convert.ToInt32(YeartextBox.Text);
+                
+                dataAdapter.Update(dataset, "StudentInfo");
+                MessageBox.Show("Successfully Updated");
+            }
+            else
+            {
+                MessageBox.Show("Duplicate Entry");
+            }         
+        }
+
+        private void Deletebutton_Click(object sender, EventArgs e)
+        {
+              string sql = "Select * From StudentInfo";
+              OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, con);
+              OleDbCommandBuilder cmdbuilder = new OleDbCommandBuilder(dataAdapter);
+              DataSet dataset = new DataSet();
+              dataAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+              dataAdapter.Fill(dataset, "StudentInfo");
+
+              DataRow findrow = dataset.Tables["StudentInfo"].Rows.Find(IDtextBox.Text);
+              if (findrow != null)
+              {
+                  findrow.Delete();
+                  dataAdapter.Update(dataset, "StudentInfo");
+                  MessageBox.Show("Student ID Successfully Deleted");
+              }
+              else
+              {
+                  MessageBox.Show("Student ID Not found");
+              }
+             
+
+      
+        }          
+
+    }
+}
